c020e254a470939b3e757b8e8bcafe793ad57ce8
fhinaatikasuri <115769425+fhinaatikasuri@users.noreply.github.com>
2022-11-25 08:39:05 +0700

    Delete MODUL3 FHINA directory

--- MODUL3 FHINA/config/connector.php
+++
@@ -1,6 +0,0 @@
-<?php
-$connector = new mysqli("localhost" . ":" . "3315" , "root", "", "modul3");
-
-if (!$connector) {
-  die("Koneksi Gagal: " . $connector->connect_error);
-}
--- MODUL3 FHINA/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-require './pages/Home-Fhina.php';
